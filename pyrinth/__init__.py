951f0afb506166d0c57bbe629e04bd72cc43e512
mythitorium <berdoncodes@shaw.ca>
2022-08-24 00:41:48 -0700

    Progress, removed cache

--- pyrinth/__init__.py
+++ pyrinth/__init__.py
@@ -6,6 +6,13 @@ import requests
 import json
 
 PATH = "https://api.modrinth.com/v2/"
+'''
+I made this because the date of creation and latest modification for projects returned via a get project call and via search call are named differently for some reason.
+Because of how I dynamically assign attributes to my container classes, this (and other inconsistencies I have yet to find)
+will end up as attributes for my classes, which I don't want
+
+This just replaces the bad key names with better ones, fixing the issue
+'''
 ALT_ANAMES = {
     "date_created" : "created_at", 
     "date_updated" : "updated_at",
@@ -36,23 +43,24 @@ class Project:
     Represents a modrinth project, either a mod or a modpack
     '''
     def __init__(self, input):
-        for key in input.keys(): 
-            setattr(self, key, input[key])
+        attribute_init(self, input, "Project")
 
 
 class Team:
-    pass
     '''
     Represents a modrinth team
     A team is a collection of one or more users who own/manage a project
     '''
+    def __init__(self, input):
+        attribute_init(self, input, "Team")
     
 
 class User:
     '''
     Represents a modrinth user
-    
     '''
+    def __init__(self, input):
+        attribute_init(self, input, "User")
 
 
 class SearchResult:
@@ -76,11 +84,20 @@ class ProjectListing():
     search query project data isn't compatible with the regular Project class. That's the gap this class fills
     '''
     def __init__(self, input):
-        # Easy iteration of attributes and their values
-        for key in input.keys():
-            # Checks for alt attribute name to use instead of the input's
-            if key in ALT_ANAMES.keys():
-                attribute = ALT_ANAMES[key]
-            else:
-                attribute = key
-            setattr(self, attribute, input[key])
+        attribute_init(self, input, "ProjectListing")
+
+
+def attribute_init(target_class, input_dict, class_type):
+    '''
+    Function that handles assigning mass attributes to container classes
+    In a function instead of the class init's themselves so I don't have to manage the same lines of code across a million places
+
+    TO DO: Error checking
+    '''
+    for key in input_dict.keys():
+        # Checks for alt attribute name to use instead of the input's
+        if key in ALT_ANAMES.keys():
+            attribute = ALT_ANAMES[key]
+        else:
+            attribute = key
+        setattr(target_class, attribute, input_dict[key])
