06376b0785115bab606a8502664bad1d77a35ef0
mythitorium <57642722+mythitorium@users.noreply.github.com>
2022-09-13 12:47:39 -0700

    Updated validator

--- swagrinth/handler.py
+++ swagrinth/handler.py
@@ -4,7 +4,7 @@ handler.py
 this acts as a mini-module which validates the contents of arguments and objects
 '''
 
-def validate_object(objects, expected_type):
+def old_validate_object(objects, expected_type):
     '''
     Used to validate the arguments for functions. Catches bad data before it's used
 
@@ -13,4 +13,22 @@ def validate_object(objects, expected_type):
     for obj in objects:
         # Check
         if not isinstance(obj, expected_type):
-            raise InvalidValue(f'Invalid type: expected {expected_type}, got {type(obj)}')
+            raise TypeError(f'Invalid type: expected {expected_type}, got {type(obj)}')
+
+
+
+def validate_objects(object_table):
+    '''
+    Used to validate the arguments for functions. Catches bad data before it's used
+    Version 4
+
+    Expected format:
+    {
+        tag : (object, expected_type)
+    }
+    '''
+    for item in object_table:
+        obj = object_table[item][0]
+        expected_type = object_table[item][1]
+        if not isinstance(obj, expected_type):
+            raise TypeError(f'Invalid type: with {item}; expected {expected_type}, got {type(obj)}')
