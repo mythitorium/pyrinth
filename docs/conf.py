d0cee5ee2f488d3937de1c6c601646f60f431eae
mythitorium <57642722+mythitorium@users.noreply.github.com>
2022-09-22 00:05:00 -0700

    Theme fix attempt

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import furo 
+import sphinx_book_theme
 import sys
 import os
 
